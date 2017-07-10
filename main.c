1df5be607b88d9b18eba19deb652b0f6ba1a7f6f
Griffin Mareske <gmareske@gmail.com>
2017-07-10 16:14:15 -0500

    Interpreter works with no loop support

--- main.c
+++ main.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int main(int argc, char **argv) {
-  char *buf = "++--+-+-f";
+// pre-condition: buf is a null-terminated string
+//   representing a brainfuck program
+void bf_eval(char *buf) {
   int pos = 0;
-  char mem[30000]; // classic brainfuck allocation space
+  unsigned char mem[30000]; // classic brainfuck allocation space
   int mempos = 0;
+  char inc; // used for user input
   while (buf[pos] != '\0') {
+
     switch(buf[pos]) {
     case '+':
+      mem[mempos]++;
       break;
     case '-':
+      mem[mempos]--;
       break;
     case '>':
+      mempos++;
       break;
     case '<':
+      mempos--;
       break;
     case '[':
       break;
     case ']':
       break;
     case '.':
+      printf("%c", mem[mempos]);
       break;
     case ',':
+      scanf(" %c", &inc);
+      mem[mempos] = inc;
       break;
     default:
       printf("unknown token '%c' at position %i\n", buf[pos], pos);
     }
+
     pos++;
   }
+}
+int main(int argc, char **argv) {
+  bf_eval(",>,<.>.");
 }
