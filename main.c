f5870bcd9f6d67d978b4b26b082b1e44b9f322d1
Griffin Mareske <gmareske@gmail.com>
2017-07-11 09:48:26 -0500

    Initialize memory cells to 0
    Commit also contains a commented-out implementation of loops
    that, if uncommented, causes a stack overflow.

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MEM_MAX 30000 // classic brainfuck number of memory cells
 // pre-condition: buf is a null-terminated string
 //   representing a brainfuck program
 void bf_eval(char *buf) {
   int pos = 0;
-  unsigned char mem[30000]; // classic brainfuck allocation space
+  unsigned char *mem[MEM_MAX];
+  for (int i = 0; i < MEM_MAX; i++) {
+    mem[i] = 0; // initialize all memory cells to 0 to start
+  }
   int mempos = 0;
   char inc; // used for user input
+  /* int stack[1024]; // stack for loops, maximum of 1024 nested loops */
+  /* int stackpos = 0; */
   while (buf[pos] != '\0') {
 
     switch(buf[pos]) {
@@ -24,8 +30,21 @@ void bf_eval(char *buf) {
       mempos--;
       break;
     case '[':
+      /* if (mem[mempos] == 0) { */
+      /*	while (buf[pos] != ']') { */
+      /*	  pos++; */
+      /*	} */
+      /*	stackpos--; */
+      /* } else { */
+      /*	stackpos++; */
+      /*	stack[stackpos] = pos; */
+      /* } */
       break;
     case ']':
+      /* if (mem[mempos] != 0) { */
+      /*	pos = stack[stackpos] - 1; */
+      /*	stackpos--; */
+      /* } */
       break;
     case '.':
       printf("%c", mem[mempos]);
@@ -42,5 +61,6 @@ void bf_eval(char *buf) {
   }
 }
 int main(int argc, char **argv) {
-  bf_eval(",>,<.>.");
+  char *helloworld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    bf_eval(helloworld);
 }
