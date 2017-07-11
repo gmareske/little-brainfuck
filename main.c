c1666120c5deb39d72bd81c682a237086d249baa
Griffin Mareske <gmareske@gmail.com>
2017-07-11 09:55:33 -0500

    Fix type error in mem array

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 #define MEM_MAX 30000 // classic brainfuck number of memory cells
+
 // pre-condition: buf is a null-terminated string
 //   representing a brainfuck program
 void bf_eval(char *buf) {
   int pos = 0;
-  unsigned char *mem[MEM_MAX];
+  unsigned char mem[MEM_MAX];
   for (int i = 0; i < MEM_MAX; i++) {
     mem[i] = 0; // initialize all memory cells to 0 to start
   }
   int mempos = 0;
-  char inc; // used for user input
-  /* int stack[1024]; // stack for loops, maximum of 1024 nested loops */
-  /* int stackpos = 0; */
+  unsigned char inc; // used for user input
   while (buf[pos] != '\0') {
 
     switch(buf[pos]) {
@@ -30,21 +29,8 @@ void bf_eval(char *buf) {
       mempos--;
       break;
     case '[':
-      /* if (mem[mempos] == 0) { */
-      /*	while (buf[pos] != ']') { */
-      /*	  pos++; */
-      /*	} */
-      /*	stackpos--; */
-      /* } else { */
-      /*	stackpos++; */
-      /*	stack[stackpos] = pos; */
-      /* } */
       break;
     case ']':
-      /* if (mem[mempos] != 0) { */
-      /*	pos = stack[stackpos] - 1; */
-      /*	stackpos--; */
-      /* } */
       break;
     case '.':
       printf("%c", mem[mempos]);
@@ -62,5 +48,5 @@ void bf_eval(char *buf) {
 }
 int main(int argc, char **argv) {
   char *helloworld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-    bf_eval(helloworld);
+  bf_eval(helloworld);
 }
