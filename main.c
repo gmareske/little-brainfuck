dd569666dbcb74ba058f93da544d78a600dacd19
Griffin Mareske <gmareske@gmail.com>
2017-07-12 11:13:59 -0500

    Add stack implementation
    Stack has been tested with push and pop and is clean in valgrind

--- main.c
+++ main.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MEM_MAX 30000 // classic brainfuck number of memory cells
 
+typedef struct bfnode {
+  int val;
+  struct bfnode *next;
+} bfnode_t;
+typedef struct bfstack {
+  bfnode_t *first;
+} bfstack_t;
+
+bfnode_t *make_node(int val) {
+  bfnode_t *n = malloc(sizeof(bfnode_t));
+  n->val = val;
+  n->next = NULL;
+  return n;
+}
+void free_node(bfnode_t *n) {
+  free(n);
+  return;
+}
+bfstack_t *make_stack() {
+  bfstack_t *st = malloc(sizeof(bfstack_t));
+  st->first = NULL;
+  return st;
+}
+void free_stack(bfstack_t *st) {
+  if (!st->first) { free(st); return;}
+  bfnode_t *cur = st->first;
+  while (cur !=NULL) {
+    bfnode_t *temp = cur->next;
+    free(cur);
+    cur = temp;
+  }
+  free(st);
+  return;
+}
+
+void push_stack(bfstack_t *st, int val) {
+  bfnode_t *n = make_node(val);
+  n->next = st->first;
+  st->first = n;
+}
+int pop_stack(bfstack_t *st) {
+  bfnode_t *n  = st->first;
+  int ret = n->val;
+  st->first = n->next;
+  free_node(n);
+  return ret;
+}
+void print_stack(bfstack_t *st) {
+  bfnode_t *cur = st->first;
+  while (cur != NULL ) {
+    printf("%d ", cur->val);
+    cur = cur->next;
+  }
+  printf("\n");
+}
 
 // pre-condition: buf is a null-terminated string
 //   representing a brainfuck program
